a7f1983444d95e527853544d60ad9d9daa8ef1a5
Charmaine <73358608+ccl19@users.noreply.github.com>
2023-02-22 12:51:18 +0000

    can display on screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
+#include <bitset>
+
 
 //Constants
   const uint32_t interval = 100; //Display update interval
@@ -79,6 +81,72 @@ void setup() {
   Serial.println("Hello World");
 }
 
+uint8_t readCols(){
+  int c0, c1, c2, c3;
+  uint8_t res;
+  // digitalWrite(RA0_PIN, LOW);
+  // digitalWrite(RA1_PIN, LOW);
+  // digitalWrite(RA2_PIN, LOW);
+  // digitalWrite(REN_PIN, HIGH);
+  c0 = digitalRead(C0_PIN);
+  c1 = digitalRead(C1_PIN);
+  c2 = digitalRead(C2_PIN);
+  c3 = digitalRead(C3_PIN);
+  res = 0;
+  res |= (c0 << 3);
+  res |= (c1 << 2);
+  res |= (c2 << 1);
+  res |= c3;
+
+  return(res);
+}
+
+void setRow(uint8_t rowIdx){
+  digitalWrite(REN_PIN, LOW);
+  if (rowIdx == 0){
+    digitalWrite(RA0_PIN, LOW);
+    digitalWrite(RA1_PIN, LOW);
+    digitalWrite(RA2_PIN, LOW);
+  }
+  else if (rowIdx == 1){
+    digitalWrite(RA0_PIN, HIGH);
+    digitalWrite(RA1_PIN, LOW);
+    digitalWrite(RA2_PIN, LOW);
+  }
+  else if (rowIdx == 2){
+    digitalWrite(RA0_PIN, LOW);
+    digitalWrite(RA1_PIN, HIGH);
+    digitalWrite(RA2_PIN, LOW);
+  }
+  else if (rowIdx == 3){
+    digitalWrite(RA0_PIN, HIGH);
+    digitalWrite(RA1_PIN, HIGH);
+    digitalWrite(RA2_PIN, LOW);
+  }
+  else if (rowIdx == 4){
+    digitalWrite(RA0_PIN, LOW);
+    digitalWrite(RA1_PIN, LOW);
+    digitalWrite(RA2_PIN, HIGH);
+  }
+  else if (rowIdx == 5){
+    digitalWrite(RA0_PIN, HIGH);
+    digitalWrite(RA1_PIN, LOW);
+    digitalWrite(RA2_PIN, HIGH);
+  }
+  else if (rowIdx == 6){
+    digitalWrite(RA0_PIN, LOW);
+    digitalWrite(RA1_PIN, HIGH);
+    digitalWrite(RA2_PIN, HIGH);
+  }
+  else if (rowIdx == 7){
+    digitalWrite(RA0_PIN, HIGH);
+    digitalWrite(RA1_PIN, HIGH);
+    digitalWrite(RA2_PIN, HIGH);
+  }
+  digitalWrite(REN_PIN, HIGH);
+
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
   static uint32_t next = millis();
@@ -86,16 +154,48 @@ void loop() {
 
   if (millis() > next) {
     next += interval;
-
+    uint8_t keyArray[7];
+    std::string keyStrArray[7];
     //Update display
     u8g2.clearBuffer();         // clear the internal memory
     u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
-    u8g2.drawStr(2,10,"Helllo World!");  // write something to the internal memory
+    u8g2.drawStr(2,10,"Hello World!");  // write something to the internal memory
+    int rowIdx = 0;
+    for (int i = 0; i < 3; i++){
+      setRow(i);
+      delayMicroseconds(3);
+      uint8_t keys = readCols();
+      std::bitset<4> myBinary(keys);
+      std::string binaryString = myBinary.to_string();
+      binaryString.c_str();
+      keyStrArray[i] = binaryString;
+      
+    }
+  
+    // Serial.println(keys);
     u8g2.setCursor(2,20);
-    u8g2.print(count++);
+    // u8g2.print(keyArray[0], BIN);
+    // u8g2.print(keyArray[1], BIN);
+    // u8g2.print(keyArray[2], BIN);
+    Serial.print(keyStrArray[0].c_str());
+    Serial.print(keyStrArray[1].c_str());
+    Serial.println(keyStrArray[2].c_str());
+
+    std::string con = keyStrArray[0]+ keyStrArray[1] + keyStrArray[2];
+
+    u8g2.drawStr(2,20, con.c_str());
+    // u8g2.drawStr(10,20, keyStrArray[1].c_str());
+    // u8g2.drawStr(10,20, keyStrArray[2].c_str());
+    //u8g2.setCursor(2,20);
+    //u8g2.print(count++);
+    
+
+
+
     u8g2.sendBuffer();          // transfer internal memory to the display
 
     //Toggle LED
     digitalToggle(LED_BUILTIN);
+    
   }
 }
